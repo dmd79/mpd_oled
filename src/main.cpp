e14b838c0b780f5acf15099e39ac09c4acf43742
dmd79 <davidevinavil@gmail.com>
2021-03-07 00:32:46 +0100

    Add display auto power off when in stop mode

--- src/main.cpp
+++ src/main.cpp
@@ -418,6 +418,11 @@ void draw_clock(ArduiPi_OLED &display, const display_info &disp_info)
   draw_date(display, 32, 56, 1, disp_info.date_format);
 }
 
+void draw_blank(ArduiPi_OLED &display, const display_info &disp_info)
+{
+  display.clearDisplay();
+}
+
 void draw_spect_display(ArduiPi_OLED &display, const display_info &disp_info)
 {
   const int H = 8; // character height
@@ -449,12 +454,21 @@ void draw_spect_display(ArduiPi_OLED &display, const display_info &disp_info)
 
 void draw_display(ArduiPi_OLED &display, const display_info &disp_info)
 {
-  mpd_state state = disp_info.status.get_state();
+  static Counter counter;
+  enum mpd_state state = disp_info.status.get_state();
   if (state == MPD_STATE_UNKNOWN || state == MPD_STATE_STOP ||
       (state == MPD_STATE_PAUSE && disp_info.pause_screen == 's'))
-    draw_clock(display, disp_info);
+    {
+      if (counter.secs() < 30)
+        draw_clock(display, disp_info);
+      else
+        draw_blank(display, disp_info);
+    }
   else
-    draw_spect_display(display, disp_info);
+    {
+      draw_spect_display(display, disp_info);
+      counter.reset();
+    }
 }
 
 namespace {
