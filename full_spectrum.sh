4cd0636ffc5822833c1b4fec00a81f849d0ea1f8
dmd79 <davidevinavil@gmail.com>
2021-09-19 03:45:55 +0200

    Add vu meter function

--- full_spectrum.sh
+++ full_spectrum.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-config='-o 1 -b 32 -A 0 -G 16 -g 1 -f 30 -R -t 120 -c alsa,plughw:Loopback,1'
+config='-o 1 -A 0 -f 30 -R -t 120 -c alsa,plughw:Loopback,1'
 F=$(head -n 1 /home/pi/mpd_oled/F.txt)
 T=$(head -n 1 /home/pi/mpd_oled/T.txt)
+e=$(head -n 1 /home/pi/mpd_oled/e.txt)
+b=$(head -n 1 /home/pi/mpd_oled/b.txt)
+G=$(head -n 1 /home/pi/mpd_oled/G.txt)
+g=$(head -n 1 /home/pi/mpd_oled/g.txt)
 
 if [ $F = '0' ]
 then
-  sudo mpd_oled_service_edit $config -F 1 -T $T
+  sudo mpd_oled_service_edit $config -F 1 -T $T -e $e -b $b -G $G -g $g
   echo "1" > /home/pi/mpd_oled/F.txt
 fi
 
 if [ $F = '1' ]
 then
-  sudo mpd_oled_service_edit $config -F 2 -T $T
+  sudo mpd_oled_service_edit $config -F 2 -T $T -e $e -b $b -G $G -g $g
   echo "2" > /home/pi/mpd_oled/F.txt
 fi
 
 if [ $F = '2' ]
 then
-  sudo mpd_oled_service_edit $config -F 0 -T $T
+  sudo mpd_oled_service_edit $config -F 0 -T $T -e $e -b $b -G $G -g $g
   echo "0" > /home/pi/mpd_oled/F.txt
 fi
